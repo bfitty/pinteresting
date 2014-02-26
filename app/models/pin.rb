a99894739ed86c4589ffbcb812a9bce85cb08437
Ben Fitterman <ben.fitterman@gmail.com>
2014-02-26 14:20:43 -0500

    Fixed upload photo error

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,9 +1,12 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, 
-		:style => { :medium => "300x300>", :thumb => "100x100>" }
-
-	validates :image, presence: true
-  validates :description, presence: true
+	has_attached_file :image,
+		:styles => {
+		  :tiny => "25x25#",
+		  :thumbnail => "100x100#",
+		  :small  => "150x150>",
+		  :medium => "300x300>" },
+		  :default_url => "/images/default_:style.png"
+	validates_attachment_content_type :image, :content_type => /\Aimage/
 end
